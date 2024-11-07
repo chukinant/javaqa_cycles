ba2a2ba8efc89789eb4c8fd87992b0e2b858df68
Anton Chukin <anton.chukin5@gmail.com>
2024-11-07 13:02:30 +0300

    tests are parameterized

--- src/test/java/ru/netology/javaqa/cycles/services/LeisureServiceTest.java
+++ src/test/java/ru/netology/javaqa/cycles/services/LeisureServiceTest.java
@@ -2,25 +2,19 @@ package ru.netology.javaqa.cycles.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 
 public class LeisureServiceTest {
 
-    @Test
-    public void ShouldCalc1() {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/LeisureServiceTestData.csv")
+    public void testCalcLeisureService(int income, int expenses, int threshold, int expected) {
         LeisureService service = new LeisureService();
-        int actual = service.calcLeisure(10_000, 3_000, 20_000);
-        int expected = 3;
+        int actual = service.calcLeisure(income, expenses, threshold);
 
         Assertions.assertEquals(actual,expected);
     }
 
-    @Test
-    public void ShouldCalc2() {
-        LeisureService service = new LeisureService();
-        int actual = service.calcLeisure(100_000, 60_000, 150_000);
-        int expected = 2;
-
-        Assertions.assertEquals(actual,expected);
-    }
 }
