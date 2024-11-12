67f85beefb7f1b713acc6af31ecfcbe81b0ee684
Anton Chukin <anton.chukin5@gmail.com>
2024-11-12 23:11:17 +0300

    code reformatted, curly brackets added

--- src/main/java/ru/netology/javaqa/cycles/services/LeisureService.java
+++ src/main/java/ru/netology/javaqa/cycles/services/LeisureService.java
@@ -2,20 +2,19 @@ package ru.netology.javaqa.cycles.services;
 
 public class LeisureService {
 
-//    int income = 170;
-//    int expenses = 80;
-//    int threshold = 100;
-
     public int calcLeisure(int income, int expenses, int threshold) {
         // initial balance
         int balance = 0;
         // variable for leisure month counting
         int count = 0;
-        for (int month=0; month < 12 ; month++) {
+
+        for (int month = 0; month < 12; month++) {
             if (balance > threshold) {
                 count += 1;
-                balance = (balance - expenses) / 3;}
-            else balance += income - expenses;
+                balance = (balance - expenses) / 3;
+            } else {
+                balance += income - expenses;
+            }
         }
         return count;
     }
